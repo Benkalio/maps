f2bef2b422f4f5505107556c3eef4d46a88eeec9
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-04-25 21:00:02 +0100

    added faker module and initialized the object to hold our variables

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,6 @@
 import faker from '@faker-js/faker';
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
@@ -8,6 +8,10 @@ class User {
   };
 
   constructor() {
-    
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
   }
 }
