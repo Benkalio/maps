722c2fa90a453bd1772fdd7de4af9f59d39b08db
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-04-27 00:25:50 +0100

    rendering google map through a separate file and called in index

---
+++ src/CustomMap.ts
@@ -0,0 +1,15 @@
+
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+  
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+}
