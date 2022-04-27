bc1579562fe173080c92e647ba73e8f1ac547e25
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-04-27 19:11:05 +0100

    refactoring

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -14,17 +14,23 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: User | Company): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
-    })
+    });
   }
 
-  addCompanyMaker(company: Company): void {
-    
-  }
+  // addCompanyMaker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   });
+  // }
 }
